6291b940de62ca96ce2f1ff024ddd743863ee574
jmperez89 <jmperez@sumamexico.com>
2020-04-29 19:25:50 -0500

    Práctiquita rápida

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ import ReactDOM from 'react-dom';
 const root = document.getElementById("root");
 //const elemento = React.createElement(componente, propiedades, hijos);
 //const elemento = React.createElement("h1", {className: "saludo"}, "Hola Mundo");
+const msj = document.getElementById("msj");
+
 
 const elemento = <h1 className="Saludos">Hola Mundo con JSX</h1>
 
+const mensaje = <p className="message">Soy un parrafo, creado con JSX</p>
+
 ReactDOM.render(elemento, root);
+
+ReactDOM.render(mensaje, msj)
