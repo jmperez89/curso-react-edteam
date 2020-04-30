a29a9f4f51548d437e092efbc5018b0072ed754e
jmperez89 <jmperez@sumamexico.com>
2020-04-29 20:35:54 -0500

    Clase 2.3 - Restricciones JSX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import "./styles/styles.scss";
 
-//const App = () => <h1>Hola mundo</h1>
+const App = () => (
+  <>
+    <div className="Saludo">
+      <h1>Hola mundo</h1>
+      <p>Hola, saludando desde mi componente</p>
+    </div>
+    <div>
+      <img src="https://statics-uestudio.uecdn.es/buhomag/2019/08/Marvel-peor-reparto-posible.jpg" alt="Avengers"/>
+    </div>
+  </>
+)
 
 export default App;
+
+// Reglas JSX
+// 1: Toda etiqueta debe cerrarse
+// 2: Los componentes deben devolver UN solo elemento padre
+// 3: Apoyarse de los Fragment cuando necesito devolver 2 elementos
+// 4: Fragment => <> hijos </>
+// 5: img siempre se cierra <img src="" alt="" />
+// 6: class => className
+// 7: El atributo for => htmlFor
+// 8: Dentro de JSX no podemos usar if, else, while
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
+import App from './App'
 
 const root = document.getElementById("root");
-//const elemento = React.createElement(componente, propiedades, hijos);
-//const elemento = React.createElement("h1", {className: "saludo"}, "Hola Mundo");
-const msj = document.getElementById("msj");
 
-
-const elemento = <h1 className="Saludos">Hola Mundo con JSX</h1>
-
-const mensaje = <p className="message">Soy un parrafo, creado con JSX</p>
-
-ReactDOM.render(elemento, root);
-
-ReactDOM.render(mensaje, msj)
+ReactDOM.render(<App />, root);
