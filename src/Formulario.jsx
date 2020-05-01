8fd5b4857c1eb92c75a7d2b9edabddf5bf75c0a5
Juan Manuel Pérez <jmperez@sumamexico.com>
2020-05-01 10:23:57 -0500

    Clase 4.2 - Formulario

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from "react"
 
 class Formulario extends Component {
-    
     // eslint-disable-next-line no-useless-constructor
     constructor(props) {
         super(props)
@@ -11,6 +10,19 @@ class Formulario extends Component {
         return (
             <div className="ed-grid">
                 <h1>Formulario</h1>
+                <form>
+                    <div className="form__item">
+                        <label>Nombre Completo</label>
+                        <input type="text"/>
+                    </div>
+                    <div className="form__item">
+                        <label>Correo Electrónico</label>
+                        <input type="email"/>
+                    </div>
+                    <div className="form__item">
+                        <input className="button full big" type="submit" value="Enviar"/>
+                    </div>
+                </form>
             </div>
         )
     }
