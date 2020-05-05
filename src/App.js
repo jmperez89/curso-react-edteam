264336affd82d1a89944e90cff01804e75163702
Juan Manuel Pérez <jmperez@sumamexico.com>
2020-05-04 23:29:57 -0500

    Clase 5.1 - Resumen del primer Curso

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import "./styles/styles.scss";
 import Banner from "./Banner"
-import Formulario from "./Formulario"
+// import Formulario from "./Formulario"
+import CourseGrid from './CourseGrid';
+// import Curso from "./Curso"
 
 const App = () => (
   <>
     <Banner />
-    <Formulario name="EDteam"/>
+    <CourseGrid />
   </>
 )
 
