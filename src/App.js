fbd1b07ad30ffc74b7ded3288585ae50d68398fa
Juan Manuel Pérez <jmperez@sumamexico.com>
2020-05-05 07:29:08 -0500

    Clase 5.5 - Parámetros de la Ruta

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import "./styles/styles.scss";
 import Banner from "./Banner"
 import Formulario from "./Formulario"
-import CourseGrid from './CourseGrid';
+import CourseGrid from "./CourseGrid";
 // import Curso from "./Curso"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Course from "./Course"
 
 const App = () => (
   <Router>
     <Switch>
       <Route path="/" exact component={Banner}/>
-      <Route path="/cursos" component={CourseGrid}/>
       <Route path="/cursos/:id" component={Course}/>
+      <Route path="/cursos" component={CourseGrid}/>
       <Route path="/formulario" component={() => <Formulario name="Pagina"/>}/>
       <Route component={() => (
         <div className="ed-grid">
---
+++ src/Course.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Course = props => (
+    <div className="ed-grid m-grid-3">
+        <h1 className="m-cols-3">Nombre del Curso</h1>
+        <img className="m-cols-1" src="https://edteam-media.s3.amazonaws.com/community/original/f6fcc152-9975-47c5-abd6-209b9ec86cc5.jpg" alt="infografía" />
+        <p className="m-cols-2">Descripción</p>
+    </div>
+)
+
+export default Course
