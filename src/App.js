69233440963615f16b6eabfeead4daf30a6401d6
Juan Manuel Pérez <jmperez@sumamexico.com>
2020-05-05 07:16:53 -0500

    Clase 5.4 - Switch y página 404

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import Banner from "./Banner"
 import Formulario from "./Formulario"
 import CourseGrid from './CourseGrid';
 // import Curso from "./Curso"
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 const App = () => (
   <Router>
-    <Route path="/" exact component={Banner}/>
-    <Route path="/cursos" exact component={CourseGrid}/>
-    <Route path="/formulario" exact component={() => <Formulario name="Pagina"/>}/>
-    <Route component={() => (
-      <div className="ed-grid">
-        <h1>Error 404</h1>
-        <span>Página no encontrada</span>
-      </div>
-    )} />
+    <Switch>
+      <Route path="/" exact component={Banner}/>
+      <Route path="/cursos" component={CourseGrid}/>
+      <Route path="/cursos/:id" component={Course}/>
+      <Route path="/formulario" component={() => <Formulario name="Pagina"/>}/>
+      <Route component={() => (
+        <div className="ed-grid">
+          <h1>Error 404</h1>
+          <span>Página no encontrada</span>
+        </div>
+      )} />
+    </Switch>
   </Router>
 )
 
