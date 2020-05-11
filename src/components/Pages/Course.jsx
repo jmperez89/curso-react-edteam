6d0fdf72ab904fac075d6b5c9174901efafe789a
Juan Manuel Pérez <jmperez@sumamexico.com>
2020-05-11 17:34:13 -0500

    Clase 8.4 - Utilizando el hook useEffect

--- src/components/Pages/Course.jsx
+++ src/components/Pages/Course.jsx
@@ -1,21 +1,16 @@
-import React, {useState} from "react"
-
+import React, {useState, useEffect} from "react"
+import Axios from 'axios'
 const Course = ({ match }) => {
 
     const [state, setState] = useState({
-        id: 1,
-        titulo: "React Desde Cero",
-        image: "https://edteam-media.s3.amazonaws.com/courses/original/5eaaec0b-fa78-4f48-94b6-61b757e1f5fa.png",
-        price: 10,
-        profesor: "Beto Quiroga 1"
+
     })
 
-    const changeTitle = (text) => {
-        setState({
-            ...state,
-            titulo: text
-        })
-    }
+    useEffect(() => {
+        console.log("hola")
+        Axios.get('https://my-json-server.typicode.com/jmperez89/json-db/courses/6')
+            .then(response => setState(response.data))
+    }, [])
 
     return(
         <div className="ed-grid m-grid-3">
@@ -25,7 +20,6 @@ const Course = ({ match }) => {
                         <h1 className="m-cols-3">{ state.titulo }</h1>
                         <img className="m-cols-1" src={state.image} alt={state.titulo} />
                         <p className="m-cols-2">Profesor: {state.profesor}</p>
-                        <button onClick={changeTitle.bind(this, "Go desde cero")}>Cambiar Titulo</button>
                     </>
                     ) :
                     <h3>El curso no Existe</h3>
