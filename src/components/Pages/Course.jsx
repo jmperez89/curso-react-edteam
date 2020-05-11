0551338f40ab42dc96efdff0921e7931817deec9
Juan Manuel Pérez <jmperez@sumamexico.com>
2020-05-11 18:49:25 -0500

    Clase 8.5 - Componentes con multiples estados

--- src/components/Pages/Course.jsx
+++ src/components/Pages/Course.jsx
@@ -1,27 +1,36 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import Axios from 'axios'
 const Course = ({ match }) => {
 
-    const [state, setState] = useState({
-
-    })
+    const [state, setState] = useState({})
+    const [comment, setComment] = useState("Sin comentarios")
 
     useEffect(() => {
-        console.log("hola")
-        Axios.get('https://my-json-server.typicode.com/jmperez89/json-db/courses/6')
+        Axios.get(`https://my-json-server.typicode.com/jmperez89/json-db/courses/${match.params.id}`)
             .then(response => setState(response.data))
     }, [])
 
-    return(
+    const myComment = e => {
+        setComment(e.target.value)
+    }
+
+    return (
         <div className="ed-grid m-grid-3">
             {
-                state ? ( 
-                    <>
-                        <h1 className="m-cols-3">{ state.titulo }</h1>
-                        <img className="m-cols-1" src={state.image} alt={state.titulo} />
-                        <p className="m-cols-2">Profesor: {state.profesor}</p>
-                    </>
-                    ) :
+                state ? (
+                    <div className="ed-grid">
+                        <div className="l-block">
+                            <h1 className="m-cols-3">{state.titulo}</h1>
+                            <img className="m-cols-1" src={state.image} alt={state.titulo} />
+                            <p className="m-cols-2">Profesor: {state.profesor}</p>
+                        </div>
+                        <div className="">
+                            <h2>Escribe tu comentario</h2>
+                            <input type="text" placeholder="Escribe..." onChange={myComment.bind(this)}></input>
+                            <p>{comment}</p>
+                        </div>
+                    </div>
+                ) :
                     <h3>El curso no Existe</h3>
             }
         </div>
